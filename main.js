a841ab6d369c11186b2e3355a2544f72805b17d6
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-12 06:38:10 +0300

    add HW

---
+++ main.js
@@ -0,0 +1,135 @@
+
+
+document.getElementById("name1").onmouseout = function () {
+  this.textContent = "Ghaida Aloufi";
+};
+
+
+document.getElementById("name2").onmouseout = function () {
+  this.textContent = "Ghaida Aloufi";
+};
+
+document.getElementById("name3").onmouseout = function () {
+  this.textContent = "Ghaida Aloufi";
+};
+
+document.getElementById("My specialty").onmouseout = function () {
+  this.textContent = "Web Developer";
+};
+
+
+document.getElementById("work1").onmouseout = function () {
+  this.textContent = "Volunteer in King Salman City";
+};
+
+document.getElementById("work2").onmouseout = function () {
+  this.textContent = "Tuwaiq camp";
+};
+
+
+
+document.getElementById("work3").onmouseout = function () {
+  this.textContent = "Taibah University";
+};
+
+
+document.getElementById("num1").onmouseover = function () {
+  this.textContent = "0542155170";
+};
+document.getElementById("num2").onmouseover = function () {
+  this.textContent = "0542155170";
+};
+
+
+document.getElementById("num3").onmouseover = function () {
+  this.textContent = "0542155170";
+};
+
+document.getElementById("email1").onmouseover = function () {
+  this.textContent = "ghaida2219@gmail.com";
+};
+
+
+document.getElementById("email2").onmouseover = function () {
+  this.textContent = "ghaida2219@gmail.com";
+};
+document.getElementById("email3").onmouseout = function () {
+  this.textContent = "ghaida2219@gmail.com";
+};
+
+
+document.getElementById("profile").onmouseover = function () {
+  this.textContent = " I am a dedicated and passionate Web Developer with a strong background in Front-end technologies. With a degree in Computer Science, I specialize in creating responsive and user-friendly websites using HTML, CSS, and JavaScript. My recent project involved developing a web application for ordering and delivering medications, showcasing my skills in building practical solutions. I am proficient in various programming languages and continually seek to enhance my skills and stay updated with the latest industry trends.";
+};
+
+document.getElementById("adderes").onmouseout = function () {
+  this.textContent = "Medina, Al-Qaswa";
+};
+
+document.getElementById("year1").onmouseout = function () {
+  this.textContent = "2017-2022";
+};
+
+document.getElementById("year2").onmouseout = function () {
+  this.textContent = "2024-2024";
+};
+
+document.getElementById("university2").onmouseout = function () {
+  this.textContent = "Tuwaiq Academy";
+};
+
+document.getElementById("x").onmouseout = function () {
+  this.textContent = "Web Developer";
+};
+
+
+document.getElementById("university").onmouseout = function () {
+  this.textContent = "Taibah University";
+};
+document.getElementById("bachelore").onmouseout = function () {
+  this.textContent = "Bachelor of Computer Science";
+};
+
+document.getElementById("gpa").onmouseout = function () {
+  this.textContent = "GPA: 4.63 / 5.0";
+};
+
+
+document.getElementById("skills1").onmouseout = function () {
+  this.textContent = "Effective Communication";
+};
+
+
+document.getElementById("skills2").onmouseout = function () {
+  this.textContent = "Teamwork";
+};
+
+
+document.getElementById("skills3").onmouseout = function () {
+  this.textContent = "Team Management";
+};
+
+
+document.getElementById("skills4").onmouseout = function () {
+  this.textContent = "Self-education";
+};
+
+
+document.getElementById("skills5").onmouseout = function () {
+  this.textContent = "Problem solvin";
+};
+
+
+document.getElementById("skills6").onmouseout = function () {
+  this.textContent = "Microsoft Office";
+};
+
+
+document.getElementById("skills7").onmouseout = function () {
+  this.textContent = "Relationship Managemen";
+};
+
+
+
+
+
